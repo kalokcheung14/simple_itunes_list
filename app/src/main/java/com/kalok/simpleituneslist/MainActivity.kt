d0bc83ffc9c3ef257a4d74f27a775dc67cd43f39
Kalok Cheung <kalokcheung14@gmail.com>
2022-04-05 13:51:23 +0800

    Update: First commit code base

---
+++ app/src/main/java/com/kalok/simpleituneslist/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.kalok.simpleituneslist
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.kalok.simpleituneslist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+     binding = ActivityMainBinding.inflate(layoutInflater)
+     setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navigationHost =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navigationHost.navController
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_bookmarks))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
