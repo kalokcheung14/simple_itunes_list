109f79e23f26e16ff4a8d2f1abcb0c5c8f0046ce
Kalok Cheung <kalokcheung14@gmail.com>
2022-04-07 14:22:38 +0800

    Fix: Removed nested subscribe in HomeViewModel

--- app/src/main/java/com/kalok/simpleituneslist/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/kalok/simpleituneslist/ui/home/HomeViewModel.kt
@@ -2,11 +2,13 @@ package com.kalok.simpleituneslist.ui.home
 
 import android.util.Log
 import androidx.lifecycle.*
+import com.kalok.simpleituneslist.models.Album
 import com.kalok.simpleituneslist.repositories.DatabaseHelper
 import com.kalok.simpleituneslist.repositories.RetrofitApiDataRepository
 import com.kalok.simpleituneslist.viewmodels.AlbumViewModel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.toObservable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 open class HomeViewModel : ViewModel() {
@@ -25,44 +27,39 @@ open class HomeViewModel : ViewModel() {
         val repo = RetrofitApiDataRepository.api
 
         // Subscribe to API call response
+        var networkAlbumList: List<Album> = emptyList()
         repo.getAlbums()
             .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ albumResponse ->
-                // Read bookmarked albums from database
-                DatabaseHelper.getAlbumDao()?.apply {
-                    getAll().subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe({ bookmarkList ->
-                            // Get album list from response and replace the current list
-                            val albumList = ArrayList<AlbumViewModel>()
-                            // add result to an array list
-                            albumList.addAll(albumResponse.results.map { album ->
-                                // Encapsulate album with ViewModel
-                                AlbumViewModel(
-                                    album,
-                                    // Update bookmarked flag based on whether the album is also contained in the bookmarked database
-                                    bookmarkList.contains(album),
-                                    compositeDisposable
-                                )
-                            })
+            .concatMap {
+                // Save API album list
+                networkAlbumList = it.results
 
-                            // Update list
-                            albums.value = albumList
-                        }, {
-                            // Handle error
-                            Log.d("album", it.message ?: "No error message")
-                        }).let {
-                            // Store disposable API call in composite disposable object
-                            compositeDisposable.add(it)
-                        }
-                }
-            }, { err ->
+                // Read bookmarked albums from database
+                DatabaseHelper.getAlbumDao()!!.getAll().toObservable()
+            }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({ bookmarkList ->
+                // Get album list from response and replace the current list
+                val albumList = ArrayList<AlbumViewModel>()
+                // add result to an array list
+                albumList.addAll(networkAlbumList.map { album ->
+                    // Encapsulate album with ViewModel
+                    AlbumViewModel(
+                        album,
+                        // Update bookmarked flag based on whether the album is also contained in the bookmarked database
+                        bookmarkList.contains(album),
+                        compositeDisposable
+                    )
+                })
+
+                // Update list
+                    albums.value = albumList
+            }, {
                 // Handle error
-                Log.d("album", err.message ?: "No error message")
-            }).let { disposable ->
+                Log.d("album", it.message ?: "No error message")
+            }).let {
                 // Store disposable API call in composite disposable object
-                compositeDisposable.add(disposable)
+                compositeDisposable.add(it)
             }
     }
 
